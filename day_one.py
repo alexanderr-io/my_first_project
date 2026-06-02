53bbd27110ee0dd14d306d3b998e1016a9c6c51c
alexander-io <tochialexanderr@gmail.com>
2026-06-02 11:43:40 -0700

    first commit

---
+++ day_one.py
@@ -0,0 +1,26 @@
+# print('Hello World') this line explains commenting....
+var1 = 'obi is a boy'
+print(var1)
+downloader_image = 'e go be naa'
+print(downloader_image)
+first_name = 'Alexander'
+course_name = 'python backend'
+lga_origin = 'osisioma'
+date_of_birth = '6-2-2002'
+religion = 'christianity'
+student_id = 'tr3/std/pyt028'
+exam_score = '68'
+print(first_name)
+print(course_name)
+print(lga_origin)
+print(date_of_birth)
+print(religion)
+print(student_id)
+print(exam_score)
+basket = {'apple', 'orange', 'apple', 'pear', 'orange', 'banana'}
+print(basket)
+is_religion_important = True
+print(is_religion_important)
+# i dont grasp booleans at all......
+# i tried writing codes with variables and strings i used variables,strings etc ......
+# i also tried to print the vars and strings i created above and it worked fine without any errors
