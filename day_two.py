5658a1b64ed20477f9fa4d77676430efba87a4af
alexander-io <tochialexanderr@gmail.com>
2026-06-04 14:38:47 -0700

    today hot oo!!

--- day_two.py
+++ day_two.py
@@ -1,1 +1,17 @@
 print(bool('0'))
+x = 15
+y = 4
+print(x + y)
+print(x - y)
+print(x * y)
+print(x / y)
+print(x % y)
+print(x ** y)
+print(x // y)
+b = 10
+b %= 3
+print(b)
+x = y = 5
+print(x is y)
+list1 = ["orange", "banana", "cherry"]
+print(type(list1))
--- new_file.py
+++ new_file.py
@@ -3,5 +3,3 @@ if name == 'Certified':
     print("you are a Python dev")
 else:
     print("gerraout!!!!")
-students_num = 25
-print(students_num)
